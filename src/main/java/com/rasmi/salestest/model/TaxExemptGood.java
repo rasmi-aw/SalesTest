fefb97dbda8cdded9e98b727a543b65345d55c45
rasmi-aw <abdelwadoudrasmi@gmail.com>
2023-11-13 11:27:01 +0100

    Refactoring, moved BigDecimal to math utils method, for single utility principle

--- src/main/java/com/rasmi/salestest/model/TaxExemptGood.java
+++ src/main/java/com/rasmi/salestest/model/TaxExemptGood.java
@@ -5,7 +5,7 @@ import com.rasmi.salestest.enums.GoodType;
 /**
  * @author Abdelouadoud Rasmi
  * This is the class representation of a tax exempted Goods, which means the taxt rate
- * will be 0 for all subclasses or objects
+ * will be 0 for all subclasses or objects, unless taxRate() was overidden
  */
 public class TaxExemptGood extends Good {
 
@@ -13,6 +13,9 @@ public class TaxExemptGood extends Good {
         super(name, price, quantity, goodType, imported);
     }
 
+    /**
+     * TaxExempt goods like Books, Medical items & Food has 0 tax rate
+     */
     @Override
     public double taxRate() {
         return 0;
